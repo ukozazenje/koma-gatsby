718cefa27ccbce108468d90b6476ff0a7c0a2e5d
zeko04 <mzecevic04@gmail.com>
2019-02-14 01:15:59 +0100

    Fix: input form not displaying email field

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,9 +10,9 @@ export default function Contact() {
       <div className="contact-container">
         <form name="contact" data-netlify="true" data-netlify-honeypot="bot-field" action="" method="post" className="contact-form">
         <input type="hidden" name="bot-field" />
-          <input type="text" name="name" placeholder="Ime" />
-          <input type="email" name="email" placeholder="Email" />
-            <textarea name="" id="" cols="30" rows="8"></textarea>
+          <input type="text" name="name" placeholder="Ime" id="name" />
+          <input type="text" name="email" placeholder="Email" id="email" />
+          <textarea name="message" id="message" cols="30" rows="8"></textarea>
             <button className="contact-button">Send</button>
         </form>
       </div>
